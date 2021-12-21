e039b57c3d6b455fa948ba0780ac2339bc8e1731
Mathéo <matheo.galu56@gmail.com>
2021-12-21 20:31:30 +0100

    footer

--- src/js/githubDashboard.js
+++ src/js/githubDashboard.js
@@ -7,16 +7,38 @@ $(document).ready(function () {
       data.forEach(function (element) {
         // append element to row
         var repo =
-          `<div class="repo-card">
+          `
+          <div class="repo-card">
           <div class="repo-card-header">
             <h2>
               ${element.name}
-            </h2>`;
+            </h2>
+            `;
+        // Fork icon
         if (element.fork) {
-          repo += `<i class="fas fa-code-branch"></i>`;
+          repo +=
+            `
+            <i class="fas fa-code-branch"></i>
+            `;
+        }
+        // Licence icon
+        if (element.license) {
+          $.ajax({
+            url: element.license.url,
+            success: function (license) {
+              repo +=
+                `
+                <a href="${license.html_url}">
+                  <i class="fas fa-balance-scale"></i>
+                </a>
+                `;
+            }
+          });
         }
         repo +=
-          `<i class="fab fa-github" ></i>
+          `<a href="${element.html_url}" target="_blank">
+            <i class="fab fa-github" ></i>
+          </a>
           </div >
             <hr>
               <div class="repo-card-content">
