e989fadc19ddc956d89a03b657818cfa4a5f2f71
Mathéo <matheo.galu56@gmail.com>
2022-01-21 01:02:47 -0500

    carroussel main behavior is OK

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ Run the docker container
 ```bash
 docker run -p 8881:8881 -v .:/usr/src/app -d paracetamol56/portfolio
 ```
+
+Or run with pm2
+```bash
+pm2 start index.js --name Portfolio --watch
+```
--- public/js/carroussel.js
+++ public/js/carroussel.js
@@ -1,55 +1,42 @@
-const $ = selector => {
-    return document.querySelector(selector);
-};
+const carroussel = document.querySelector(".carroussel");
 
-// setInterval(() => {
+// const interval = setInterval(() => {
 //     next();
-// }, 5000);
+// }, 10000);
 
 function next() {
     console.log("next");
 
     // Put last element of the carroussel in the first position
-    console.log($(".carroussel").children);
-    $(".carroussel").insertBefore($(".carroussel").lastElementChild, $(".carroussel").firstElementChild);
-    console.log($(".carroussel").children);
-
-    // for each element in the carroussel
-    // assign classe hide to every carroussel child
-    for (let i = 0; i < $(".carroussel").children.length; i++) {
-        $(".carroussel").children[i].classList = "hide carroussel-card";
-    }
+    carroussel.insertBefore(carroussel.lastElementChild, carroussel.firstElementChild);
 
     // Assing classes to the 3 first elements
-    $(".carroussel").children[0].classList = "next carroussel-card";
-    $(".carroussel").children[1].classList = "act carroussel-card";
-    $(".carroussel").children[2].classList = "prev carroussel-card";
+    carroussel.children[1].classList = "act carroussel-card";
+
+    carroussel.children[0].classList = "next carroussel-card";
+
+    carroussel.children[2].classList = "prev carroussel-card";
+    carroussel.children[3].classList = "hide carroussel-card";
 }
 
 function prev() {
     console.log("prev");
 
     // Put first element of the carroussel in the last position
-    console.log($(".carroussel").children);
-    $(".carroussel").appendChild($(".carroussel").firstElementChild);
-    console.log($(".carroussel").children);
-
-    // for each element in the carroussel
-    // assign classe hide to every carroussel child
-    for (let i = 0; i < $(".carroussel").children.length; i++) {
-        $(".carroussel").children[i].classList = "hide carroussel-card";
-    }
+    carroussel.appendChild(carroussel.firstElementChild);
 
     // Assing classes to the 3 first elements
-    $(".carroussel").children[0].classList = "next carroussel-card";
-    $(".carroussel").children[1].classList = "act carroussel-card";
-    $(".carroussel").children[2].classList = "prev carroussel-card";
+    carroussel.lastElementChild.classList = "hide carroussel-card";
+    carroussel.children[0].classList = "next carroussel-card";
+    carroussel.children[1].classList = "act carroussel-card";
+    carroussel.children[2].classList = "prev carroussel-card";
 }
 
-$(".prev").addEventListener("click", () => {
-    prev();
-});
-
-$(".next").addEventListener("click", () => {
-    next();
+// Event listener on carroussel click
+carroussel.addEventListener("click", function (e) {
+    if (e.target.classList.contains("next")) {
+        next();
+    } else if (e.target.classList.contains("prev")) {
+        prev();
+    }
 });
