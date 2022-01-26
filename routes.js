dcb408029a9121d6799c21608c60c369f0e3f4be
Mathéo <matheo.galu56@gmail.com>
2022-01-26 01:03:27 -0500

    Fix '&#39;next&#39; is defined but never used.' issue in routes.js

--- routes.js
+++ routes.js
@@ -8,7 +8,7 @@ router.get('/', function (req, res, next) {
 });
 
 /* about page. */
-router.get('/about', function (req, res, next) {
+router.get('/about', function (req, res) {
     res.render('about');
 });
 
