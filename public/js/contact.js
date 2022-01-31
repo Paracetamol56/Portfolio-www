dca8e7723ac3d495d9bc750af11a903ddeb742dc
Mathéo <matheo.galu56@gmail.com>
2022-01-30 20:52:25 -0500

    Contact feedback text

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,9 +1,15 @@
 document.getElementById('contact-form').addEventListener('submit', function (e) {
+    const stateFeedback = document.getElementById('state-feedback');
+
     var json = {
         "name": document.getElementById('name').value,
         "email": document.getElementById('email').value,
         "message": document.getElementById('message').value
     };
+
+    stateFeedback.innerHTML = 'Envoi en cours...';
+
+    document.getElementById('contact-form-submit').disabled = true;
 
     fetch('/contact/sendmail', {
         method: 'POST',
@@ -13,6 +19,12 @@ document.getElementById('contact-form').addEventListener('submit', function (e) 
         body: JSON.stringify(json)
     })
         .then(function (response) {
+            document.getElementById('contact-form').reset();
+            if (response.ok) {
+                stateFeedback.innerHTML = 'Votre message a bien été envoyé !';
+            } else {
+                stateFeedback.innerHTML = 'Une erreur est survenue, veuillez réessayer plus tard.';
+            }
             return response.json();
         })
 
