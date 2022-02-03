8fcddca33b2a89e989f7e84ef22e47b15e7e155c
Mathéo <matheo.galu56@gmail.com>
2022-02-03 14:20:52 -0500

    project parsing : title and icon

--- public/js/projectParser.js
+++ public/js/projectParser.js
@@ -1,17 +1,49 @@
-// project class
-class Project {
-    constructor(jsonURL) {
-        this.projectData = {};
-        // Use ajax to get the json file
-        $.getJSON(jsonURL, function (data) {
-            this.projectData = data;
-        });
-    }
+// function to get the json data from the server with the id in parameter
+function renderProject(id) {
+    // get the json data from the server
+    $.getJSON("/content/projects/" + id + ".json", function (data) {
+        data = data[0];
+        console.log(data);
 
-    render() {
-        console.log(this.projectData);
-    }
+        // Title and subtitle
+        const title = $("#titles").find("h1");
+        const subtitle = $("#titles").find("h2");
+        title.html(title.html().replace("{{title}}", data.title));
+        subtitle.html(subtitle.html().replace("{{subtitle}}", data.subtitle));
+
+        // Info section
+        const infoContainer = $("#info").find(".info-container");
+        // for each element in data.infos
+        $.each(data.infos, function (index, value) {
+            var icon = "<i class=\"fas fa-question\"></i>";
+            if (value.type === "fa") {
+                icon = "<i class=\"fas " + value.icon + "\"></i>";
+            }
+            else if (value.type === "svg") {
+                icon = value.icon;
+            }
+
+            console.log(icon);
+
+            infoContainer.append(
+                "<div class=\"project-info\">" +
+                "    <div class=\"project-info-background\"></div>" +
+                "    <h3>" + index + "</h3>" +
+                icon +
+                "    <p>" + value.text + "</p>" +
+                "</div>"
+            );
+        });
+    });
 }
 
-var project = new Project("/content/projects/0.json");
-project.render();
+// Get the ip parameter in the page uri
+function getParameterByName(name) {
+    var url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    return url.match(new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)")) ?
+        decodeURIComponent(url.match(new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"))[2]) :
+        null;
+};
+
+renderProject(getParameterByName('id'));
