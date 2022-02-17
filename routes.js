75e92a202800f92d1a963d0c08636592144503c4
Mathéo <matheo.galu56@gmail.com>
2022-02-17 10:36:59 -0500

    Updates pages structures

--- routes.js
+++ routes.js
@@ -7,11 +7,6 @@ router.get('/', function (req, res) {
     res.render('index');
 });
 
-// about page.
-router.get('/about', function (req, res) {
-    res.render('about');
-});
-
 // contact page.
 router.get('/contact', function (req, res) {
     res.render('contact');
