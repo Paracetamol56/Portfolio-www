eff3eef8933a3ac5a414eb88474efef834f6f382
Mathéo <matheo.galu56@gmail.com>
2022-03-28 13:52:09 -0400

    Templated project row

--- public/js/projectRowParser.js
+++ public/js/projectRowParser.js
@@ -1,12 +1,16 @@
 function renderProjects() {
     // use ajax to get all the projects from the api /content/projects
     // and render them in the projects page
-    const projectContainer = $("#projects").find(".container");
-
     $.ajax({
         url: '/content/projects',
         type: 'GET',
         success: function (data) {
+            // Get the project container
+            const projectContainer = $("#projects").find(".container");
+            // Get the project template
+            const projectTemplate = projectContainer.html();
+            $(projectContainer).empty();
+
             $.each(data, function (index, value) {
                 value = value[0];
                 const number = (index + 1).toString().padStart(2, '0');
@@ -21,23 +25,18 @@ function renderProjects() {
                     }
                     infos += "<li>" + icon + " " + info.text + "</li>";
                 });
+
+                // Format the template
+                var projectRow = projectTemplate
+                    .replace("{{thumbnail}}", value.image)
+                    .replace("{{number}}", number)
+                    .replace("{{title}}", value.title)
+                    .replace("{{subtitle}}", value.subtitle)
+                    .replace("{{infos}}", infos)
+                    .replace("{{link}}", "/project?id=" + index.toString());
+
                 // Append the project to the container in first position
-                projectContainer.prepend(
-                    "<div class=\"project-row\">" +
-                    "    <img src=\"" + value.image + "\" class=\"thumbnail\" alt=\"Thumbnail\">" +
-                    "    <div class=\"project-description\">" +
-                    "        <div class=\"section-title\">" +
-                    "            <h2><span class=\"section-title-number\">" + number + "</span> " + value.title + "</h2>" +
-                    "            <hr>" +
-                    "            <h3>" + value.subtitle + "</h3>" +
-                    "        </div>" +
-                    "        <ul class=\"project-description-list\">" + infos + "</ul>" +
-                    "        <a class=\"project-link\" href=\"/project?id=" + index.toString() + "\">VOIR PLUS" +
-                    "            <i class=\"fas fa-chevron-right\"></i>" +
-                    "        </a>" +
-                    "    </div>" +
-                    "</div>"
-                );
+                $(projectContainer).prepend(projectRow);
             });
         }
     });
