9051a5dba8a2f2391eeb272b5717977747d501df
Mathéo <matheo.galu56@gmail.com>
2022-07-13 12:45:40 +0200

    Remade loader annimation

--- src/components/global/LoaderAnimation.vue
+++ src/components/global/LoaderAnimation.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- Loader -->
   <div id="loader" class="loader-wrapper">
-    <svg
+    <!-- <svg
       id="Calque_2"
       class="loader-logo"
       data-name="Calque 2"
@@ -25,6 +25,303 @@
         points="667.98 270 346.07 827.65 387.85 900 512.1 900 875.86 269.95 667.98 270"
       />
       <polygon class="cls-2" points="0 900 180 900 180 540 0 180 0 900" />
+    </svg> -->
+    <svg
+      class="loader-logo"
+      xmlns:cc="http://creativecommons.org/ns#"
+      xmlns:dc="http://purl.org/dc/elements/1.1/"
+      xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+      xmlns:svg="http://www.w3.org/2000/svg"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      viewBox="0 0 1588 1122.7"
+    >
+      <polygon
+        class="st0"
+        points="581.9,739 581.9,436.6 669.2,436.6 775.4,620.5 859.8,474.4 1035.5,474.4 1035.5,739 837.9,739 
+	881.6,663.4 959.9,663.4 959.9,549.9 903.4,549.9 794.3,739 756.5,739 657.5,567.5 657.5,739 "
+      />
+      <path
+        id="path20_00000139974231263478493260000014802661880912911535_"
+        class="st0"
+        d="M1035.5,436.6V361H794.3l-62.6,108.3
+	l43.7,75.6l62.5-108.3H1035.5"
+      />
+      <polygon
+        class="st1"
+        points="581.9,739 657.5,739 657.5,567.5 581.9,436.6 "
+      />
+      <polygon
+        class="st1"
+        points="731.7,696.1 756.5,739 794.3,739 947,474.4 859.8,474.4 "
+      />
+      <path
+        id="path20"
+        class="st2"
+        d="M1035.5,436.6V361H794.3l-62.6,108.3l43.7,75.6l62.5-108.3H1035.5"
+      />
+      <path
+        id="path22"
+        class="st2"
+        d="M581.9,739h75.6V567.5l99,171.4h37.8l109.1-189h56.5v113.4h-78.3L837.9,739h197.6V474.4H859.8
+	l-84.3,146.1L669.2,436.6h-87.3V739"
+      />
+      <g id="g98">
+        <path id="path294" class="st3" d="M653.5,560.7L471.9,246" />
+        <path id="path296" class="st3" d="M665.2,429.8L537.4,208.3" />
+        <path id="path298" class="st3" d="M487.3,246.3l42.6-24.6" />
+        <g id="g300">
+          <path
+            id="path302"
+            class="st4"
+            d="M486.2,244.3l2.2,3.8l-12.6,4.7L486.2,244.3z"
+          />
+        </g>
+        <g id="g304">
+          <path
+            id="path306"
+            class="st4"
+            d="M528.7,219.8l2.2,3.8l10.3-8.5L528.7,219.8z"
+          />
+        </g>
+        <text
+          transform="matrix(0.8663 -0.4995 0.4995 0.8663 496.9567 235.7044)"
+          class="st5"
+        >
+          1
+        </text>
+        <text
+          transform="matrix(0.8663 -0.4995 0.4995 0.8663 505.8571 230.5719)"
+          class="st5"
+        >
+          0
+        </text>
+        <path id="path312" class="st3" d="M1043.4,739h308.3" />
+        <path id="path314" class="st3" d="M1043.4,474.4h308.3" />
+        <path id="path316" class="st3" d="M1343.7,725.7V487.6" />
+        <g id="g318">
+          <path
+            id="path320"
+            class="st4"
+            d="M1341.5,725.7h4.4l-2.2,13.3L1341.5,725.7z"
+          />
+        </g>
+        <g id="g322">
+          <path
+            id="path324"
+            class="st4"
+            d="M1341.5,487.6h4.4l-2.2-13.2L1341.5,487.6z"
+          />
+        </g>
+        <text
+          transform="matrix(6.906525e-07 -1 1 6.906525e-07 1338.7148 618.0243)"
+          class="st5"
+        >
+          3
+        </text>
+        <text
+          transform="matrix(6.906525e-07 -1 1 6.906525e-07 1338.7148 607.7499)"
+          class="st5"
+        >
+          5
+        </text>
+        <path id="path330" class="st3" d="M1043.4,474.4h308.3" />
+        <path id="path332" class="st3" d="M1043.4,436.6h308.3" />
+        <path id="path334" class="st3" d="M1343.7,461.2v-11.4" />
+        <g id="g336">
+          <path
+            id="path338"
+            class="st4"
+            d="M1341.5,461.2h4.4l-2.2,13.2L1341.5,461.2z"
+          />
+        </g>
+        <g id="g340">
+          <path
+            id="path342"
+            class="st4"
+            d="M1341.5,449.9h4.4l-2.2-13.3L1341.5,449.9z"
+          />
+        </g>
+        <text
+          transform="matrix(6.906525e-07 -1 1 6.906525e-07 1338.715 461.704)"
+          class="st5"
+        >
+          5
+        </text>
+        <path id="path348" class="st3" d="M1043.4,436.6h308.3" />
+        <path id="path350" class="st3" d="M1043.4,361.1h308.3" />
+        <path id="path352" class="st3" d="M1343.7,423.4v-49.1" />
+        <g id="g354">
+          <path
+            id="path356"
+            class="st4"
+            d="M1341.5,423.4h4.4l-2.2,13.2L1341.5,423.4z"
+          />
+        </g>
+        <g id="g358">
+          <path
+            id="path360"
+            class="st4"
+            d="M1341.5,374.3h4.4l-2.2-13.2L1341.5,374.3z"
+          />
+        </g>
+        <text
+          transform="matrix(6.906525e-07 -1 1 6.906525e-07 1338.715 410.7436)"
+          class="st5"
+        >
+          1
+        </text>
+        <text
+          transform="matrix(6.906525e-07 -1 1 6.906525e-07 1338.715 400.4691)"
+          class="st5"
+        >
+          0
+        </text>
+        <path id="path366" class="st3" d="M665.2,429.8L537.4,208.3" />
+        <path id="path368" class="st3" d="M727.8,462.5L570.1,189.3" />
+        <path id="path370" class="st3" d="M552.8,208.5l9.8-5.7" />
+        <g id="g372">
+          <path
+            id="path374"
+            class="st4"
+            d="M551.7,206.6l2.2,3.8l-12.6,4.7L551.7,206.6z"
+          />
+        </g>
+        <g id="g376">
+          <path
+            id="path378"
+            class="st4"
+            d="M561.5,200.9l2.2,3.8l10.4-8.6L561.5,200.9z"
+          />
+        </g>
+        <text
+          transform="matrix(0.8682 -0.4961 0.4961 0.8682 551.0351 204.5036)"
+          class="st5"
+        >
+          5
+        </text>
+        <path id="path384" class="st3" d="M574,436.6H260.2" />
+        <path id="path386" class="st3" d="M574,739H260.2" />
+        <path id="path388" class="st3" d="M268.1,449.9v275.8" />
+        <g id="g390">
+          <path
+            id="path392"
+            class="st4"
+            d="M265.8,449.9h4.4l-2.2-13.3L265.8,449.9z"
+          />
+        </g>
+        <g id="g394">
+          <path
+            id="path396"
+            class="st4"
+            d="M265.8,725.7h4.4l-2.2,13.3L265.8,725.7z"
+          />
+        </g>
+        <text
+          transform="matrix(6.906525e-07 -1 1 6.906525e-07 263.1147 598.8234)"
+          class="st5"
+        >
+          4
+        </text>
+        <text
+          transform="matrix(6.906525e-07 -1 1 6.906525e-07 263.1147 588.549)"
+          class="st5"
+        >
+          0
+        </text>
+        <path id="path402" class="st3" d="M581.9,746.9v45.8" />
+        <path id="path404" class="st3" d="M657.5,746.9v45.8" />
+        <path id="path406" class="st3" d="M595.1,784.8h49.2" />
+        <g id="g408">
+          <path
+            id="path410"
+            class="st4"
+            d="M595.1,782.6v4.4l-13.2-2.2L595.1,782.6z"
+          />
+        </g>
+        <g id="g412">
+          <path
+            id="path414"
+            class="st4"
+            d="M644.3,782.6v4.4l13.2-2.2L644.3,782.6z"
+          />
+        </g>
+        <text transform="matrix(1 0 0 1 609.4403 780.3373)" class="st5">1</text>
+        <text transform="matrix(1 0 0 1 619.7147 780.3373)" class="st5">0</text>
+        <path id="path420" class="st3" d="M837.9,746.9v79.4" />
+        <path id="path422" class="st3" d="M1035.4,746.9v79.4" />
+        <path id="path424" class="st3" d="M851.1,818.3h171.1" />
+        <g id="g426">
+          <path
+            id="path428"
+            class="st4"
+            d="M851.1,816.1v4.4l-13.2-2.2L851.1,816.1z"
+          />
+        </g>
+        <g id="g430">
+          <path
+            id="path432"
+            class="st4"
+            d="M1022.2,816.1v4.4l13.2-2.2L1022.2,816.1z"
+          />
+        </g>
+        <text transform="matrix(1 0 0 1 914.2401 813.8578)" class="st5">2</text>
+        <text transform="matrix(1 0 0 1 924.5145 813.8578)" class="st5">6</text>
+        <text transform="matrix(1 0 0 1 934.7889 813.8578)" class="st5">.</text>
+        <text transform="matrix(1 0 0 1 939.9266 813.8578)" class="st5">1</text>
+        <text transform="matrix(1 0 0 1 950.201 813.8578)" class="st5">3</text>
+        <path id="path438" class="st3" d="M1035.4,466.5V158.4" />
+        <path id="path440" class="st3" d="M859.8,466.5V158.4" />
+        <path id="path442" class="st3" d="M1022.2,166.3H873" />
+        <g id="g444">
+          <path
+            id="path446"
+            class="st4"
+            d="M1022.2,164.1v4.5l13.2-2.2L1022.2,164.1z"
+          />
+        </g>
+        <g id="g448">
+          <path
+            id="path450"
+            class="st4"
+            d="M873,164.1v4.5l-13.2-2.2L873,164.1z"
+          />
+        </g>
+        <text transform="matrix(1 0 0 1 925.12 161.8573)" class="st5">2</text>
+        <text transform="matrix(1 0 0 1 935.3944 161.8573)" class="st5">3</text>
+        <text transform="matrix(1 0 0 1 945.6688 161.8573)" class="st5">.</text>
+        <text transform="matrix(1 0 0 1 950.8065 161.8573)" class="st5">2</text>
+        <text transform="matrix(1 0 0 1 961.0809 161.8573)" class="st5">5</text>
+        <path id="path456" class="st3" d="M771.4,627.4L582.2,955.2" />
+        <path id="path458" class="st3" d="M790.3,745.9L647.6,993.1" />
+        <path id="path460" class="st3" d="M597.6,955l42.5,24.6" />
+        <g id="g462">
+          <path
+            id="path464"
+            class="st4"
+            d="M598.6,953.1l-2.2,3.8l-10.4-8.6L598.6,953.1z"
+          />
+        </g>
+        <g id="g466">
+          <path
+            id="path468"
+            class="st4"
+            d="M641.2,977.6l-2.2,3.8l12.6,4.7L641.2,977.6z"
+          />
+        </g>
+        <text
+          transform="matrix(0.8656 0.5007 -0.5007 0.8656 612.7976 958.1065)"
+          class="st5"
+        >
+          1
+        </text>
+        <text
+          transform="matrix(0.8656 0.5007 -0.5007 0.8656 621.6912 963.2511)"
+          class="st5"
+        >
+          0
+        </text>
+      </g>
     </svg>
     <div class="loader-animation">
       <span></span>
@@ -62,6 +359,8 @@ export default {
     #2c1b4f 0%,
     #111416 100%
   ) !important;
+  background-position: center;
+  animation: background 6s infinite;
   z-index: 99;
   display: flex;
   flex-direction: column;
@@ -74,16 +373,41 @@ export default {
   }
 
   .loader-logo {
-    width: 100px;
+    width: 100%;
+    max-width: 500px;
     filter: drop-shadow(0 0 5px #5f77ef9a);
     animation: 2s var(--easing) 0s normal forwards 1 loader-fade-in;
 
-    .cls-1 {
+    .st0 {
       fill: #8251e9;
     }
-
-    .cls-2 {
+    .st1 {
       fill: #5f3cab;
+    }
+    .st2 {
+      fill: none;
+      stroke: #b4e2f9;
+      stroke-width: 1.92;
+      stroke-linecap: round;
+      stroke-linejoin: round;
+      stroke-miterlimit: 10;
+    }
+    .st3 {
+      fill: none;
+      stroke: #b4e2f9;
+      stroke-width: 0.96;
+      stroke-linecap: round;
+      stroke-linejoin: round;
+      stroke-miterlimit: 10;
+    }
+    .st4 {
+      stroke: #b4e2f9;
+      stroke-linecap: round;
+      stroke-miterlimit: 10;
+    }
+    .st5 {
+      fill: #b4e2f9;
+      font-family: "ArialMT";
     }
   }
 
@@ -121,6 +445,20 @@ export default {
 
       &:nth-child(5) {
         animation: dot-5 2s infinite;
+      }
+    }
+
+    @keyframes background {
+      0% {
+        background-size: 100%;
+      }
+
+      50% {
+        background-size: 150%;
+      }
+
+      100% {
+        background-size: 100%;
       }
     }
 
