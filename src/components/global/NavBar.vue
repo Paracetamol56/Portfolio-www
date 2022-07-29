32cfb77528666b1d693d004ceadc40cff32847eb
Mathéo <matheo.galu56@gmail.com>
2022-07-29 23:34:46 +0200

    Basic language selector
    ToDo: CSS

--- src/components/global/NavBar.vue
+++ src/components/global/NavBar.vue
@@ -109,6 +109,18 @@
           </svg>
         </label>
       </div>
+
+      <div>
+        <select v-model="$i18n.locale">
+          <option
+            v-for="locale in $i18n.availableLocales"
+            :key="`locale-${locale}`"
+            :value="locale"
+          >
+            {{ locale }}
+          </option>
+        </select>
+      </div>
     </div>
   </nav>
 </template>
