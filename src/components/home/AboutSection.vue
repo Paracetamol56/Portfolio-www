659ea2f3736bf2b964cfdbfded75421fa375f353
Mathéo <matheo.galu56@gmail.com>
2022-08-03 09:46:30 +0200

    Finished about section

--- src/components/home/AboutSection.vue
+++ src/components/home/AboutSection.vue
@@ -2,30 +2,30 @@
 {
   "en": {
     "title": "Presentation",
-    "main-paragraph": "New technologies are taking a growing place in our lives. This is why I'm currently studying computer science, a field that I want to apply to concrete causes. Curious and having the will to learn, I like to discover and get involved in new areas of technology: artificial intelligence, web, embedded systems, etc...",
+    "main-paragraph": "New technologies are taking a growing place in our lives. This is why I'm currently studying computer science, a field that I want to apply to concrete causes. Curious and having the will to learn, I like to discover and get involved in new areas of technology: artificial intelligence, web, embedded systems, etc... My long-term ambition is to work in a sector that I particularly like: aerospace. Indeed, having always been passionate about the stars, I want to participate in the development of humanity in space.",
     "occupations": {
       "1": {
-        "strong": "Computer science student",
-        "normal": " in Annecy (France)"
+        "strong": "Computer science student ",
+        "normal": "in professional bachelor's degree of multi-support IT development in Annecy (France)"
       },
       "2": {
-        "strong": "Work-study student in web development and server management",
-        "normal": " at Hunimalis in Annecy (France)"
+        "strong": "Work-study student in web development and server management ",
+        "normal": "at Hunimalis in Annecy (France)"
       },
     },
   },
   "fr": {
     "title": "Présentation",
     "main-paragraph":
-      "Les technologies sont en train de s'intéresser. C'est pourquoi aujourd'hui je suis en train de faire des études en informatique. C'est pourquoi je suis curieux et à l'esprit de découvrir et de participer à des nouveaux domaines de technologie: intelligence artificielle, web, systèmes embarqués, etc…",
+      "Les technologies sont en train de s'intéresser. C'est pourquoi aujourd'hui je suis en train de faire des études en informatique. C'est pourquoi je suis curieux et à l'esprit de découvrir et de participer à des nouveaux domaines de technologie: intelligence artificielle, web, systèmes embarqués, etc… Mon ambition sur le long terme est de travailler dans un secteur qui me tient très à cœur : l’aérospatial. En effet, ayant toujours été passionné par les étoiles, je désire participer au développement de l’humanité dans l’espace.",
     "occupations": {
       "1": {
-        "strong": "Etudiant en informatique",
-      "normal": " à Annecy"
+        "strong": "Etudiant en informatique ",
+      "normal": "en licence professionnelle de développement informatique multisupport à Annecy (France)"
       },
       "2": {
-        "strong": "Etudiant en alternance dans le développement web et la gestion de serveurs",
-        "normal": " chez Hunimalis en Annecy"
+        "strong": "Etudiant en alternance dans le développement web et la gestion de serveurs ",
+        "normal": "chez Hunimalis à Annecy (France)"
       }
     },
   }
@@ -47,12 +47,25 @@
       </div>
       <div class="about-occupation fade-in">
         <p>
-          <b>{{ $t("occupations.1.strong") }}</b>
-          {{ $t("occupations.1.normal") }}
+          <b>
+            <i class="fa-solid fa-graduation-cap"></i>
+            {{ $t("occupations.1.strong") }}
+          </b>
+          <a
+            href="https://formations-iuta.univ-smb.fr/fr/toute-l-offre-de-formation/licence-professionnelle-DP/sciences-technologies-sante-STS/licence-professionnelle-metiers-de-l-informatique-conception-developpement-et-test-logiciels-I2WYFB5X//lp-developpeur-informatique-multi-supports-alternance-I2WYLMKA.html?search-keywords=LP,D%C3%A9veloppeur,informatique,multi,supports,en,apprentissage"
+            target="_blank"
+          >
+            {{ $t("occupations.1.normal") }}
+          </a>
         </p>
         <p>
-          <b>{{ $t("occupations.2.strong") }}</b>
-          {{ $t("occupations.2.normal") }}
+          <b>
+            <i class="fa-solid fa-briefcase"></i>
+            {{ $t("occupations.2.strong") }}
+          </b>
+          <a href="https://www.hunimalis.com/" target="_blank">
+            {{ $t("occupations.2.normal") }}
+          </a>
         </p>
       </div>
     </div>
@@ -82,14 +95,6 @@ section#about {
     font-size: 1rem;
     text-align: left;
     margin: 16px 0;
-    p {
-      margin: 4px 0;
-      b {
-        background: var(--primary-color);
-        color: var(--background-color);
-        padding: 0 4px;
-      }
-    }
   }
 }
 </style>
