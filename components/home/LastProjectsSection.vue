34905540cbcfcb5c127b2b92f04ac908fd0d54eb
Mathéo Galuba <matheo.galu56@gmail.com>
2025-05-23 12:21:10 +0200

    Update home page to use collections

--- components/home/LastProjectsSection.vue
+++ components/home/LastProjectsSection.vue
@@ -12,100 +12,90 @@ const localePath = useLocalePath();
 const modules = [Navigation, Pagination];
 
 defineProps({
-	number: {
-		type: String,
-		required: true,
-	},
+  number: {
+    type: String,
+    required: true,
+  },
 });
 
 const { data } = await useAsyncData(
-	"projects-limit-4",
-	() => queryContent(locale.value).sort({ id: -1, $numeric: true }).limit(4).find()
+  "projects-limit-4",
+  () => queryCollection(locale.value)
+    .select('number', 'title', 'subtitle', 'thumbnail', 'dates', 'tags', 'status')
+    .order('number', 'DESC')
+    .limit(4)
+    .all()
 );
 </script>
 
 <template>
-	<section id="projects" class="fade-in">
-		<div class="container">
-			<div class="section-title">
-				<h2>
-					<span class="section-title-number">{{ number }}.</span>
-					{{ $t("home.projects.title") }}
-				</h2>
-				<hr />
-			</div>
-		</div>
+  <section id="projects" class="fade-in">
+    <div class="container">
+      <div class="section-title">
+        <h2>
+          <span class="section-title-number">{{ number }}.</span>
+          {{ $t("home.projects.title") }}
+        </h2>
+        <hr />
+      </div>
+    </div>
 
-		<Swiper
-			class="project-slider"
-			:slides-per-view="1.1"
-			:space-between="20"
-			:navigation="true"
-			:pagination="{ clickable: true }"
-			:loop="false"
-			:grab-cursor="true"
-			:centeredSlides="true"
-			:breakpoints="{
-				576: {
-					slidesPerView: 1.2,
-					spaceBetween: 40,
-				},
-				768: {
-					slidesPerView: 1.4,
-					spaceBetween: 60,
-				},
-				992: {
-					slidesPerView: 1.6,
-					spaceBetween: 80,
-				},
-				1200: {
-					slidesPerView: 1.8,
-					spaceBetween: 100,
-				},
-				1400: {
-					slidesPerView: 2.2,
-					spaceBetween: 100,
-				},
-			}"
-			:modules="modules"
-		>
-			<SwiperSlide
-				class="project-slider-item"
-				v-for="project in data"
-				:key="project.id"
-			>
-				<ProjectCard :project="project" />
-			</SwiperSlide>
-		</Swiper>
+    <Swiper class="project-slider" :slides-per-view="1.1" :space-between="20" :navigation="true"
+      :pagination="{ clickable: true }" :loop="false" :grab-cursor="true" :centeredSlides="true" :breakpoints="{
+        576: {
+          slidesPerView: 1.2,
+          spaceBetween: 40,
+        },
+        768: {
+          slidesPerView: 1.4,
+          spaceBetween: 60,
+        },
+        992: {
+          slidesPerView: 1.6,
+          spaceBetween: 80,
+        },
+        1200: {
+          slidesPerView: 1.8,
+          spaceBetween: 100,
+        },
+        1400: {
+          slidesPerView: 2.2,
+          spaceBetween: 100,
+        },
+      }" :modules="modules">
+      <SwiperSlide class="project-slider-item" v-for="project in data" :key="project.id">
+        <ProjectCard :project="project" />
+      </SwiperSlide>
+    </Swiper>
 
-		<div class="button-wrapper">
-			<BoxButton
-				:href="localePath('/project')"
-			>
-				{{ $t("home.projects.button") }}
-			</BoxButton>
-		</div>
-	</section>
+    <div class="button-wrapper">
+      <BoxButton :href="localePath('/project')">
+        {{ $t("home.projects.button") }}
+      </BoxButton>
+    </div>
+  </section>
 </template>
 
 <style scoped lang="scss">
 section#projects {
-	background-image: url("@/assets/img/BluryBackground.svg");
-	background-size: contain;
-	background-position: center;
-	background-repeat: no-repeat;
-	.project-slider {
-		margin: 1rem 0;
-		padding: 0;
-		display: grid;
-		grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
-		grid-gap: 1rem;
-	}
-	.button-wrapper {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		margin-top: 1rem;
-	}
+  background-image: url("@/assets/img/BluryBackground.svg");
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+
+  .project-slider {
+    margin: 1rem 0;
+    padding: 0;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+    grid-gap: 1rem;
+  }
+
+  .button-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 1rem;
+  }
 }
 </style>
