f8a396d51bf9b45936be85750e6f1cc8e955cd43
Mathéo Galuba <matheo.galu56@gmail.com>
2025-05-23 12:16:48 +0200

    Created 2 collections

--- content.config.ts
+++ content.config.ts
@@ -1,10 +1,64 @@
-import { defineContentConfig, defineCollection } from '@nuxt/content'
+import { defineContentConfig, defineCollection, z } from '@nuxt/content'
 
 export default defineContentConfig({
   collections: {
-    content: defineCollection({
+    en: defineCollection({
       type: 'page',
-      source: '**/*.md'
+      source: 'en/*.md',
+      schema: z.object({
+        number: z.number(),
+        title: z.string(),
+        subtitle: z.string(),
+        thumbnail: z.string(),
+        dates: z.object({
+          start: z.string(),
+          end: z.string(),
+        }),
+        tags: z.array(z.string()),
+        technologies: z.array(
+          z.object({
+            name: z.string(),
+            description: z.string(),
+            icon: z.string(),
+          })
+        ),
+        status: z.string(),
+        team: z.array(
+          z.object({
+            name: z.string(),
+            role: z.string(),
+          })
+        ),
+      })
+    }),
+    fr: defineCollection({
+      type: 'page',
+      source: 'fr/*.md',
+      schema: z.object({
+        number: z.number(),
+        title: z.string(),
+        subtitle: z.string(),
+        thumbnail: z.string(),
+        dates: z.object({
+          start: z.string(),
+          end: z.string(),
+        }),
+        tags: z.array(z.string()),
+        technologies: z.array(
+          z.object({
+            name: z.string(),
+            description: z.string(),
+            icon: z.string(),
+          })
+        ),
+        status: z.string(),
+        team: z.array(
+          z.object({
+            name: z.string(),
+            role: z.string(),
+          })
+        ),
+      })
     })
   }
 })
